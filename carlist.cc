4e6983dba842e12396a04f76fd9542ff85a7ffe9
Antonio López Sobreviela <als140@alu.ua.es>
2024-12-26 12:29:30 +0100

    als140: Añadido el código

---
+++ carlist.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // Lista de marcas de coches
+    vector<string> marcas = {"Toyota", "Ford", "BMW", "Audi", "Mazda", "Mecedes-Benz", "McLaren", "Nissan"};
+
+    cout << "Marcas de carros 'to guapas:\n";
+    for (size_t i = 0; i < marcas.size(); ++i) {
+        cout << i + 1 << ". " << marcas[i] << endl;
+    }
+
+    return 0;
+}
+
