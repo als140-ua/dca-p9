350b1868e0ce26f9e159136b4a6b51ffdb78aa48
Antonio López Sobreviela <als140@alu.ua.es>
2024-12-26 12:49:19 +0100

    Añadidas más marcas de coches

--- carlist.cc
+++ carlist.cc
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
     // Lista de marcas de coches
-    vector<string> marcas = {"Toyota", "Ford", "BMW", "Audi", "Mazda", "Mecedes-Benz", "McLaren", "Nissan"};
+    vector<string> marcas = {"Toyota", "Ford", "BMW", "Audi", "Mazda", "Mecedes-Benz", "McLaren", "Nissan", "DMC", "Suzuki"};
 
     cout << "Marcas de carros 'to guapas:\n";
     for (size_t i = 0; i < marcas.size(); ++i) {
